940f81280098b04bb272794fcb8dcd5d6df16987
T1it5an1icCSV <145016510+T1it5an1icCSV@users.noreply.github.com>
2024-06-07 19:16:07 +0300

    Add files via upload

---
+++ 888/888/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace _888
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static string theme;
+        public static string Theme
+        {
+            get { return theme; }
+            set
+            {
+                theme = value;
+                var dict = new ResourceDictionary { Source = new Uri($"/StyleLib;component/Themes/{value}.xaml", UriKind.Relative) };
+
+                Current.Resources.MergedDictionaries.RemoveAt(0);
+                Current.Resources.MergedDictionaries.Insert(0, dict);
+
+                var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                if (File.Exists($"{desktop}\\theme.txt"))
+                {
+                    Theme = File.ReadAllText($"{desktop}\\theme.txt");
+                }
+            }
+        }
+    }
+}
+
---
+++ 888/888/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace _888
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Ser_Button_Click(object sender, RoutedEventArgs e)
+        {
+            string textToSerialize = inputText.Text;
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string jsonFilePath = Path.Combine(desktopPath, "SerializationText.json");
+
+            File.WriteAllText(jsonFilePath, JsonConvert.SerializeObject(textToSerialize));
+
+            inputText.Text = "";
+        }
+
+        private void Des_Button_Click(object sender, RoutedEventArgs e)
+        {
+            string jsonFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "SerializationText.json");
+
+            if (File.Exists(jsonFilePath))
+            {
+                string deserializedText = File.ReadAllText(jsonFilePath);
+                if (!string.IsNullOrEmpty(deserializedText))
+                {
+                    outputText.Text = deserializedText; 
+                }
+            }
+            else
+            {
+                MessageBox.Show("Файл не найден.");
+            }
+        }
+
+
+        private void LightTheme_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var uri = new Uri(@"\Themes\Light.xaml", UriKind.Relative);
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+        }
+
+        private void DarkTheme_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var uri = new Uri(@"\Themes\Dark.xaml", UriKind.Relative);
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (inputText.Text == "Введите текст...")
+            {
+                inputText.Text = "";
+                inputText.Foreground = Brushes.Black; 
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(inputText.Text))
+            {
+                inputText.Text = "Введите текст...";
+                inputText.Foreground = Brushes.Gray; 
+            }
+        }
+
+
+    }
+}
+
